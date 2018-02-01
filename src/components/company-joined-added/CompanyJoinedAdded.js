843cd3ffc967e5192a0be207d74c8410490a561f
Anthony <30807672+Caccavella@users.noreply.github.com>
2018-02-01 14:20:58 -0700

    Update CompanyJoinedAdded.js

--- src/components/company-joined-added/CompanyJoinedAdded.js
+++ src/components/company-joined-added/CompanyJoinedAdded.js
@@ -8,7 +8,7 @@ export default function () {
     return (
         <div className='company-joined-added-body'>
             <img src={ICON_IMAGE} className='just_joined_icon'/>
-            <div className='company-joined-added-title'>You Did It! That's UTTERLY awesome!</div>
+            <div className='company-joined-added-title'>You Did It! That is UTTERLY awesome!</div>
             <div className='company-joined-added-subtitle'> Login to get started</div>
             <a href={process.env.REACT_APP_LOGIN}>
                             <div className='company-joined-created-login-button'>Login</div>
